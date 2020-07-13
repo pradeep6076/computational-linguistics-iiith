dfbb4f7e1fc25272e884aade7d74d101dd23997f
pradeep6076 <pradeepkatari12345@gmail.com>
2020-07-13 12:30:20 +0530

    goal10

--- RDIP/Codes/Functions.js
+++ RDIP/Codes/Functions.js
@@ -29,6 +29,9 @@ document.getElementById("form").innerHTML="";
 document.getElementById("wrong").innerHTML="";
 document.getElementById("correct").innerHTML="";
 document.getElementById("getAnswer").innerHTML="";
+document.getElementById("list").innerHTML="";
+document.getElementById("hide").innerHTML="";
+document.getElementById("getAnswer1").innerHTML="";
 
 
 var x= document.getElementById("choose").value;
@@ -91,7 +94,7 @@ document.getElementById("form").innerHTML= finalSentence;
    
    document.getElementById("reform").innerHTML= "<button id='reform' onclick='resets()'>"+"Re-form the sentence"+"</button>"
     if(display.length == shuffle.length){
-    			document.getElementById("correctnessBtn").innerHTML = "<button class='correctBtn' onclick='check()'>" +"Check correctness of the sentence"+ "</button>";
+    document.getElementById("correctnessBtn").innerHTML = "<button class='correctBtn' onclick='check()'>" +"Check correctness of the sentence"+ "</button>";
     }
 
   }
@@ -120,6 +123,11 @@ document.getElementById("form").innerHTML="";
 document.getElementById("wrong").innerHTML="";
 document.getElementById("correct").innerHTML="";
 document.getElementById("getAnswer").innerHTML="";
+document.getElementById("correctnessBtn").innerHTML="";
+document.getElementById("list").innerHTML="";
+document.getElementById("hide").innerHTML="";
+document.getElementById("getAnswer1").innerHTML="";
+
 	
 for (var i = 0; i < shuffle.length; i++) {
 document.getElementById("random").innerHTML += "<button id='"+i+"' class='wordBtn' onclick='my("+i+")'>" + shuffle[i] + "</button>";
@@ -185,9 +193,163 @@ document.getElementById("correct").innerHTML="Correct answer"+" "+"<i class='fa 
 } else{
 document.getElementById("correct").innerHTML="";
 document.getElementById("wrong").innerHTML="Wrong answer"+" "+"<i class='fa fa-frown-o' aria-hidden='true'></i>";
-document.getElementById("getAnswer").innerHTML="<button class='correctAnsBtn'>"+"GET CORRECT SENTENCE"+"</button>";
+document.getElementById("getAnswer").innerHTML="<button class='correctAnsBtn' onclick='get()'>"+"GET CORRECT SENTENCE"+"</button>";
 
 }
+}
+function get(){
+  correctSentences=[];
+  var count=0;
+  var isFound=false;
+ var answerExists= document.getElementById("list").textContent;
+var languageArray=document.getElementById("choose").value;
+
+if(languageArray == 'English'){
+if(answerExists == ""){ 
+  for(var k=0; k<englishArray.length+1; k++)
+   
+  {
+	
+if(isFound==true && count== englishArray[k-1][0].length)
+   {
+         
+	var z = k;
+          
+if (z==1)
+         {
+	for(p=0; p<englishArray[z-1].length;p++)
+            {
+                 correctSentences[p]=(englishArray[z-1][p]);
+                  
+               }
+            }
+		if (z>1)
+           {
+			for(p=0; p<englishArray[z-1].length;p++)
+               {
+                  correctSentences[p]=(englishArray[k-1][p]);
+                
+               }
+                   
+            }
+         
+            break;
+        }
+      for(var i=0; i<display.length; i++)
+		{
+            
+            isFound=englishArray[k][0].includes(display[i]);
+            //alert("inside for loop :"+ i +": is found flag "+isFound);
+			if(isFound==true)
+            {
+              count++;
+              
+            }
+
+            if(isFound==false)
+            {
+              count= 0;
+              break;
+              
+            }
+
+          }
+}
+  //alert(correctSentences[0]);
+    //alert(correctSentences[1]);
+
+  //alert(correctSentences);
+  for(j=0; j<correctSentences.length;j++)
+  {
+	//alert("Found correct sentences :"+correctSentences[j]);
+    document.getElementById("list").innerHTML+=correctSentences[j].join(" ")+"<br>";
+  }
+  document.getElementById("hide").innerHTML="<button class='hide' onclick='hideAnswers()'>"+ "hide the correct sentence" + "</button>";
+  document.getElementById("getAnswer").innerHTML="";
+}
+}
+
+if(languageArray == 'Hindi'){
+if(answerExists == ""){ 
+  for(var k=0; k<hindiArray.length+1; k++)   
+  {
+	
+	if(isFound==true && count== hindiArray[k-1][0].length)
+        {
+          //alert(" found is true and the first value of K is :"+k);
+			var z = k;
+          //alert("value of z is set to :" +z);
+		if (z==1)
+           {
+			for(p=0; p<hindiArray[z-1].length;p++)
+               {
+                  correctSentences[p]=(hindiArray[z-1][p]);
+                  //alert("Inside for loop to get correctSentences when z=1 and loop number is :"+p+"  : and the value of correctSentences is ; "+correctSentences);
+                  //alert("value of z is :"+z);
+               }
+            }
+		if (z>1)
+           {
+			for(p=0; p<hindiArray[z-1].length;p++)
+               {
+                  correctSentences[p]=(hindiArray[k-1][p]);
+                 // alert("Inside for loop to get correctSentences when z>1 and p loop number is :"+p+"  : and the value of correctSentences is ; "+correctSentences);
+                  //alert("value of z is :"+z);
+               }
+                   
+            }
+         
+            break;
+        }
+      	for(var i=0; i<display.length; i++)
+		{
+            //alert("Inside for loop and the first element of englishArray is : "+englishArray[k][0]);
+            //alert("inside for loop :"+ i +": and displayArray element is :"+displayArray[i]);
+            isFound=hindiArray[k][0].includes(display[i]);
+            //alert("inside for loop :"+ i +": is found flag "+isFound);
+			if(isFound==true)
+            {
+              count++;
+              //alert("Increasing Count : "+ count);
+            }
+
+            if(isFound==false)
+            {
+              count= 0;
+              break;
+              
+            }
+
+          }
+}
+  //alert(correctSentences[0]);
+    //alert(correctSentences[1]);
+
+  //alert(correctSentences);
+  for(j=0; j<correctSentences.length;j++)
+  {
+	//alert("Found correct sentences :"+correctSentences[j]);
+    document.getElementById("list").innerHTML+=correctSentences[j].join(" ")+"<br>";
+  }
+ document.getElementById("hide").innerHTML="<button class='hide' onclick='hideAnswers()'>"+ "hide the correct sentence" + "</button>";
+  document.getElementById("getAnswer").innerHTML="";
+}	
+}
+}
+
+function hideAnswers(){
+	var x=document.getElementById("list");
+	x.style.display="none";
+	document.getElementById("hide").innerHTML="";
+	document.getElementById("getAnswer1").innerHTML="<button class='correctAnsBtn' onclick='showAnswers()'>"+"Get Answers"+"</button>";
+}
+
+function showAnswers(){
+	var v=document.getElementById("list");
+	v.style.display="block";
+	document.getElementById("getAnswer1").innerHTML="";
+  document.getElementById("hide").innerHTML="<button class='hide' onclick='hideAnswers()'>"+ "hide the correct sentence" + "</button>";
+
 }
 
 
